654fb37a081c0ae855e52956a2a8207c91253268
afpatmin <patrick.minogue@appfoundry.se>
2017-04-26 22:40:34 +0200

    contact page changes
    * rearranged buttons and popup on contact page
    * added rome and re-added gothenburg
    * added jan and mauritzio profiles
    * made font more helvetica-like for header nav
    * made font more myriad pro-like for content text
    * updated texts according to content document

--- web/contact.dart
+++ web/contact.dart
@@ -1,25 +1,48 @@
 import "dart:html";
 import 'page.dart';
+import 'phrase.dart';
 
+final Phrase phrase = new Phrase("en");
+final ElementList<Element> bodies = querySelectorAll(".body");
+final ElementList<Element> cityButtons = querySelectorAll(".contact-button");
+final ElementList<Element> cityNodes = querySelectorAll(".node");
+final ElementList<Element> cityOutputs = querySelectorAll(".city-output"); //largeBodyContainer.querySelector("#large-body-city");
+final ElementList<Element> addressOutputs = querySelectorAll(".address-output"); //largeBodyContainer.querySelector("#large-body-address");
+final ElementList<Element> contactOutputs = querySelectorAll(".contact-output");//largeBodyContainer.querySelector("#large-body-contact");
+
+final NodeValidator validator = new NodeValidatorBuilder.common()..allowNavigation(new EmailUriPolicy());
 
 void main()
 {
   Page.init("contact");
 
-  querySelectorAll(".contact-button").onClick.listen(onButtonClick);
-  querySelectorAll(".node").onClick.listen(onButtonClick);
+  cityButtons.onClick.listen(onButtonClick);
+  cityNodes.onClick.listen(onButtonClick);
 }
 
 void onButtonClick(Event e)
 {
   Element source = e.target;
-  querySelectorAll(".node").classes.remove("open");
-  querySelectorAll(".body").classes.add("hide");
+  cityNodes.classes.remove("open");
+  cityButtons.classes.remove("open");
+  bodies.classes.remove("hide");
 
   String target = source.dataset["target"];
 
-  querySelector("#small-body-$target").classes.remove("hide");
-  querySelector("#large-body-$target").classes.remove("hide");
-  querySelector("#node-$target").classes.add("open");
+  cityOutputs.forEach((e) => e.setInnerHtml(phrase.get("$target")));
+  addressOutputs.forEach((e) => e.setInnerHtml(phrase.get("address_$target")));
+  contactOutputs.forEach((e) => e.setInnerHtml(phrase.get("contact_$target"), validator: validator));
 
+  querySelector("#node-$target")?.classes?.add("open");
+  querySelector("#button-$target")?.classes?.add("open");
+}
+
+class EmailUriPolicy implements UriPolicy
+{
+  EmailUriPolicy();
+
+  bool allowsUri(String uri)
+  {
+    return uri.startsWith("mailto:");
+  }
 }
